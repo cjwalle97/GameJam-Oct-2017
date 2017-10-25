6a33f668347a1427baeebc4e9c17c65809c7f10f
BlaneTorregrossa <blanet97@cox.net>
2017-10-25 15:09:19 -0500

    Simple Player Control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public GameObject Player;
+    public Transform PlayerTransform;
+
+
+	void Start ()
+    {
+		
+	}
+
+    void Update ()
+    {
+		if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.z += 0.25f;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.z -= 0.25f;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= 0.25f;
+            this.transform.position = position;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += 0.25f;
+            this.transform.position = position;
+        }
+	}
+}
