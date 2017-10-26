02afe5749b1edfb0e81eaa8bfd6fc320b7a76042
BlaneTorregrossa <blanet97@cox.net>
2017-10-26 10:14:20 -0500

    Changed for player

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+
+    public Player player;
+    public MenuOptions menuInstance;
+
+    private int playerHealth;
+    private int playerKeys;
+    private bool playerIsDead;
+
+    void Start ()
+    {
+        player.health = 1;
+        player.keys = 0;
+        player.isDead = false;
+    }
+	
+	void Update ()
+    {
+        //  Only here if we ever expand max health to go higher than 1
+        if (playerHealth == 0)
+        {
+            playerIsDead = true;
+        }
+
+        if (playerIsDead == true)
+        {
+            menuInstance.PlayerCameraCanvas.SetActive(true);
+            Destroy(player);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Key")
+        {
+            playerKeys++;
+            Debug.Log("Player touched key");
+        }
+
+        if (other.tag == "Enemy")
+        {
+            playerHealth--;
+            Debug.Log("Player was hit by enemy.");
+        }
+    }
+}
