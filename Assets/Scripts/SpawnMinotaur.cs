bf04c3af6d07814984ff91c9a9d67c3a3b3626d4
BlaneTorregrossa <blanet97@cox.net>
2017-10-26 12:30:28 -0500

    Timer + Enemy Spawn

---
+++ Assets/Scripts/SpawnMinotaur.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMinotaur : MonoBehaviour
+{
+
+    public GameObject minotaur;
+    public GameObject enemySpawn;
+    public int enemiesCreated;
+    public float timer;
+
+	void Start ()
+    {
+        timer = 0;
+	}
+	
+	void Update ()
+    {
+        timer = Time.time;
+        SpawnEnemy();
+	}
+
+    void SpawnEnemy()
+    {
+        if (timer >= 30.0f && timer <= 31.0f && enemiesCreated != 1)
+        {
+            Instantiate(minotaur, enemySpawn.transform.position, enemySpawn.transform.rotation);
+            enemiesCreated++;
+        }
+    }
+}
